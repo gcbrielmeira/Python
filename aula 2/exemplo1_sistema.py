a858bf7360bb22593756a72f289d25b24ecb8d18
gcbrielmeira <meiracruzgabriel@gmail.com>
2025-02-15 14:22:49 -0300

    atualizando

---
+++ aula 2/exemplo1_sistema.py
@@ -0,0 +1,19 @@
+
+print( "Bem vindo ao sistema do Cinema" )
+print(" Digite sua idade: ")
+idade = int(input() )  
+
+if idade >= 18 :
+    print( "pode assistir o filme")
+else:
+    print( " Nao pode assistir o filme")
+
+
+# Criar novo arquivo
+# Desenvolver um sistema
+# Valor de A, Valor de B e Valor de C
+# Calcular a Bhaskara
+# Delta = b2 - 4 * a * c
+# Baskhara 
+
+
